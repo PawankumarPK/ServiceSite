3154f753244ee621f176e322414ee8871411be98
Pawan Kumar <pyadav.workmail@gmail.com>
2023-11-29 12:08:47 +0530

    add portfolio and gurantee section

---
+++ src/components/Guarantee.jsx
@@ -0,0 +1,195 @@
+import { Box, Grid, Typography } from '@mui/material'
+import { Container } from '@mui/system'
+import file from "../../public/png/file.png";
+import rocket from "../../public/png/rocket.png";
+import lightBulbImg from "../../public/png/lightbulb.png";
+
+import React from 'react'
+import Image from 'next/image';
+
+const Guarantee = () => {
+    return (
+        <Grid bgcolor="#F7F8FA">
+
+            <Grid container direction="column" alignItems="center">
+
+                <Typography component="p" mt={10} fontSize={14} fontFamily='poppins-medium' textAlign="center" color="#27363B">
+                    What we promise
+                </Typography>
+
+                <Grid width={100} height={2} bgcolor="#40536B"></Grid>
+
+
+                <Grid container justifyContent='center' textAlign="center" mb={5}>
+                    <Typography component="h3" noWrap mt={2} fontSize={45} fontFamily="poppins-semibold" style={{ lineHeight: "60px" }}>
+                        We Gives
+                    </Typography>
+
+                    <Typography component="h3" color='#9CC118' mt={2} fontSize={45} fontFamily="poppins-semibold" style={{ lineHeight: "60px" }}>
+                        &nbsp;Guarantee
+                    </Typography>
+                </Grid>
+
+
+                <Grid container={{ lg: true, sm: false }} pb={15} pt={10} alignItems='center' justifyContent='center'>
+
+
+                    {/* ------------------------- Tell your requirements Section ------------------------*/}
+                    <Grid mr={{ lg: 5, md: 5 }} height={350} width={400} mb={5} bgcolor="#fff">
+
+                        <Box
+                            sx={{
+                                pt: 4,
+                                pl: 5,
+                                display: "flex",
+                            }}>
+
+                            <Image
+                                height={120}
+                                pt={5}
+                                sx={{ position: 'relative', pt: 5 }}
+                                src={lightBulbImg}
+                                alt='LightBulb Image'>
+                            </Image>
+
+                            <Typography sx={{ position: "absolute", pt: 9, pl: 3 }}
+                                variant='h5'
+                                fontFamily="poppins-semibold"
+                                component='subtitle1'
+                                color='#202020'
+                                fontWeight='600'
+                                textAlign='center'>
+
+                                Refund All Money
+
+                            </Typography>
+
+                        </Box>
+
+
+
+                        <Grid pt={2} pb={5}>
+                            <Typography sx={{ padding: 2 }}
+                                fontSize={13}
+                                lineHeight="22px"
+                                fontFamily="poppins-regular"
+                                component='p'
+                                color='#202020'
+                                textAlign='center'>
+
+                                We understand that there are risks involved in project management, and we are committed to working with you to deliver a product on time and in the best possible quality. But if we cannot meet your deadline, we will give you a full refund of your money.
+                            </Typography>
+                        </Grid>
+
+
+                    </Grid>
+
+
+                    {/* ------------------------- We Plan & design Section ------------------------*/}
+                    <Grid mr={{ lg: 5, md: 5 }} mb={5} height={350} width={400} bgcolor="#fff">
+
+                        <Box
+                            sx={{
+                                pt: 5,
+                                pl: 5,
+                                display: "flex",
+                            }}>
+
+                            <Image
+                                height={120}
+                                pt={5}
+                                sx={{ position: 'relative', pt: 5 }}
+                                src={file}
+                                alt='File Image'>
+                            </Image>
+
+                            <Typography sx={{ position: "absolute", pt: 9, pl: 3 }}
+                                variant='h5'
+                                fontFamily="poppins-semibold"
+                                component='subtitle1'
+                                color='#202020'
+                                fontWeight='600'
+                                textAlign='center'>
+
+                                Give Free Service
+
+                            </Typography>
+
+                        </Box>
+
+
+
+                        <Grid pt={2}>
+                            <Typography sx={{ padding: 2 }}
+                                fontSize={13}
+                                fontFamily="poppins-regular"
+                                component='p'
+                                lineHeight="22px"
+                                color='#595959'
+                                textAlign='center'>
+
+                                We are happy to offer a free service for the duration of this project. If we fail to meet the deadline, If we fail to meet the deadline, then you can cancel your order without any penalty. And you will get a full refund amount
+                            </Typography>
+                        </Grid>
+
+
+                    </Grid>
+
+
+                    {/* ------------------------- We Deliver & Launch Section ------------------------*/}
+                    <Grid height={350} width={400} mb={5} bgcolor="#fff">
+
+                        <Box
+                            sx={{
+                                pt: 5,
+                                pl: 5,
+                                display: "flex",
+                            }}>
+
+                            <Image
+                                height={120}
+                                pt={5}
+                                sx={{ position: 'relative', pt: 5 }}
+                                src={rocket}
+                                alt='Rocket Image'>
+                            </Image>
+
+                            <Typography sx={{ position: "absolute", pt: 9, pl: 3 }}
+                                variant='h5'
+                                fontFamily="poppins-semibold"
+                                component='subtitle'
+                                color='#202020'
+                                fontWeight='600'
+                                textAlign='center'>
+
+                                Written Agreement
+
+                            </Typography>
+
+                        </Box>
+
+
+
+                        <Grid pt={2}>
+                            <Typography sx={{ padding: 2 }}
+                                fontSize={13}
+                                fontFamily="poppins-regular"
+                                component='p'
+                                lineHeight="22px"
+                                color='#595959'
+                                textAlign='center'>
+
+                                We are going to give you a detailed description of the conditions that will apply to your project. This includes things like deadlines, refund clauses, and other promises.
+                                Everything is on written agreement.
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+
+
+        </Grid>
+    )
+}
+
+export default Guarantee
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,9 +5,19 @@ import "owl.carousel/dist/assets/owl.theme.default.css";
 import screenshot_one from "../../public/png/screenshot_1.png";
 import screenshot_two from "../../public/png/screenshot_2.png";
 import screenshot_three from "../../public/png/screenshot_3.png";
+import portfolio_1 from "../../public/png/portfolio_1.png";
+import portfolio_2 from "../../public/png/portfolio_2.png";
+import portfolio_3 from "../../public/png/portfolio_3.png";
+import portfolio_4 from "../../public/png/portfolio_4.png";
+import portfolio_5 from "../../public/png/portfolio_5.png";
+import portfolio_6 from "../../public/png/portfolio_6.png";
 import Image from "next/image";
-import { Grid, Link, Typography } from "@mui/material";
+import { Box, Grid, Link, Typography } from "@mui/material";
 import { motion, AnimatePresence } from 'framer-motion'
+import teamBgOne from "../../public/png/teamBg1.png";
+import teamBgTwo from "../../public/png/teamBg2.png";
+import teamBgThree from "../../public/png/teamBg3.png";
+
 
 //import OwlCarousel from "react-owl-carousel"
 
@@ -31,63 +41,103 @@ const variants = {
     }
 }
 
+///----------------- Team common sections -----------------
+const PortfolioDesc = ({ name, img, nameBg, altName }) => (
+    <Grid mt={10} mr={{ lg: 5, md: 7, sm: 5, xs: 5 }} ml={{ lg: 5, md: 7, sm: 5 }}>
+
+        <Grid justifyContent='center' alignItems="center" display='flex'
+            style={{
+                color: "#707070",
+                border: '2px solid',
+            }}
+        >
+
+            <Image
+                width={320}
+                src={img}
+                alt={altName} />
+        </Grid>
+
+        <Box
+
+            sx={{
+                pt: 2,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+
+            <Image
+                height={40}
+                sx={{ position: 'relative' }}
+                src={nameBg}
+                alt='Team BG One Image'>
+            </Image>
+
+            <Typography sx={{ position: "absolute" }}
+                fontSize={15}
+                fontFamily="poppins-semibold"
+                component='h4'
+                color='#fff'
+                textAlign='center'>
+
+                {name}
+
+            </Typography>
+
+
+        </Box>
+
+
+
+
+    </Grid>
+)
+
+
 const Portfolio = () => {
 
 
-
-
-
-    const options = {
-
-        responsive: {
-            0: {
-                items: 1,
-            },
-            400: {
-                items: 1,
-            },
-            600: {
-                items: 2,
-            },
-            700: {
-                items: 2,
-            },
-            1000: {
-                items: 3,
-            },
-        },
-    };
-
-
+    <Grid
+        ml={{ lg: 4, md: 4 }}
+        width={{ lg: 300, md: 350, sm: 300, xs: 350 }}
+        mt={{ sm: 5, xs: 5 }}>
+        <Image
+            layout='responsive'
+            priority
+            src={portfolio_1}
+            alt='study image'>
+        </Image>
+    </Grid>
     return (
 
 
-        <Grid height={800} bgcolor="#F7F8FA" id="portfolio">
+        <Grid height={1750} id="portfolio">
 
 
 
-            <Grid>
-                <Grid container direction="column" alignItems="center">
-                    <Typography component="subtitle2" pt={15} fontSize={14} fontFamily='poppins-medium' textAlign="center" color="#27363B">
-                        Our Portfolio
+            <Grid container direction="column" alignItems="center">
+                <Typography component="subtitle2" pt={15} fontSize={14} fontFamily='poppins-medium' textAlign="center" color="#27363B">
+                    Our Portfolio
+                </Typography>
+
+                <Grid width={100} height={2} bgcolor="#40536B" />
+
+
+                <Grid container justifyContent='center' textAlign="center">
+                    <Typography component="h3" color="#000" noWrap mt={2} fontSize={45} fontFamily="poppins-semibold" style={{ lineHeight: "60px" }}>
+                        Latest &
                     </Typography>
 
-                    <Grid width={100} height={2} bgcolor="#40536B" />
-
-
-                    <Grid container justifyContent='center' textAlign="center">
-                        <Typography component="h3" color="#000" noWrap mt={2} fontSize={45} fontFamily="poppins-semibold" style={{ lineHeight: "60px" }}>
-                            Latest &
-                        </Typography>
-
-                        <Typography component="h3" color='#9CC118' mt={2} fontSize={45} fontFamily="poppins-semibold" style={{ lineHeight: "60px" }}>
-                            &nbsp;Creative Work
-                        </Typography>
-                    </Grid>
-
+                    <Typography component="h3" color='#9CC118' mt={2} fontSize={45} fontFamily="poppins-semibold" style={{ lineHeight: "60px" }}>
+                        &nbsp;Creative Work
+                    </Typography>
                 </Grid>
-                <Grid mt={10}>
-                    <OwlCarousel items={3}
+
+            </Grid>
+            <Grid >
+
+                {/* <OwlCarousel items={3}
 
                         loop={true}
                         nav
@@ -160,14 +210,111 @@ const Portfolio = () => {
 
 
 
-                    </OwlCarousel>
+                    </OwlCarousel> */}
+
+
+                <Grid display='flex' justifyContent='center' >
+
+                    {/* Chemtime Academy */}
+                    <Link href="https://chemtime.co.in/" scroll={true} passHref target="_blank">
+                        <PortfolioDesc
+                            name="Chemtime"
+                            nameBg={teamBgOne}
+                            img={portfolio_1}
+                            altName="Chemtime Website">
+                        </PortfolioDesc>
+                    </Link>
+
+                    {/* Givan Academy */}
+                    <Link href="https://givan-academy.netlify.app/" scroll={true} passHref target="_blank">
+                        <PortfolioDesc
+                            name="Givan Academy"
+                            nameBg={teamBgTwo}
+                            img={portfolio_2}
+                            altName="Givan Academy Website">
+
+                        </PortfolioDesc>
+                    </Link>
+
+                    {/* IELTS Guroo Academy */}
+                    <Link href="https://ielts-guroo.netlify.app/" scroll={true} passHref target="_blank">
+                        <PortfolioDesc
+                            name="IELTS Guroo"
+                            nameBg={teamBgThree}
+                            img={portfolio_3}
+                            altName="IELTS Guroo Website">
+
+                        </PortfolioDesc>
+                    </Link>
 
                 </Grid>
+
+
+                <Grid display='flex' justifyContent='center' >
+
+                    {/* Guragon IAS Academy */}
+                    <Link href="https://ielts-guroo.netlify.app/" scroll={true} passHref target="_blank">
+                        <PortfolioDesc
+                            name="Gurgaon IAS"
+                            nameBg={teamBgOne}
+                            img={portfolio_4}
+                            altName="Gurgaon IAS Website">
+                        </PortfolioDesc>
+                    </Link>
+
+                    {/* Optimum Travel */}
+                    <PortfolioDesc
+                        name="Optimum Travel"
+                        nameBg={teamBgTwo}
+                        img={portfolio_6}
+                        altName="Optimum Travel Website">
+
+                    </PortfolioDesc>
+
+                    {/* INGTI Travel */}
+                    <Link href="https://ignti-landing-page.netlify.app/" scroll={true} passHref target="_blank">
+                        <PortfolioDesc
+                            name="IGNTI"
+                            nameBg={teamBgThree}
+                            img={portfolio_5}
+                            altName="IGNTI Website">
+
+                        </PortfolioDesc>
+                    </Link>
+
+                </Grid>
+
+
+                <Grid justifyContent='center' alignItems='center' display='flex' pt={8}>
+                    <Typography
+                        component="p"
+                        pl={{ lg: 3, xs: 1 }} pr={3} pb={{ lg: 0.5 }}
+                        fontFamily='poppins-medium'
+                        color="#595959"
+                        textTransform='none'>
+                        We’re excited to announce that we have 7 more projects coming up within the next month!
+
+                    </Typography>
+                </Grid>
+
+                <Grid justifyContent='center' alignItems='center' display='flex' pt={1}>
+                    <Typography
+                        component="p"
+                        pl={{ lg: 3, xs: 1 }} pr={3} pb={{ lg: 0.5 }}
+                        fontFamily='poppins-medium'
+                        color="#595959"
+                        textTransform='none'>
+                        We’re sure you’ll love working with us and can’t wait to see what we do next.
+
+                    </Typography>
+                </Grid>
+
             </Grid>
-
-
-
         </Grid>
+
+
+
+
     );
 
 };
